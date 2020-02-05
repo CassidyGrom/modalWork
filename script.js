d5aa5a759b704695f49bc2160a9492b45476d72f
Cassidy Grom <cgrom@njadvancemedia.com>
2020-02-05 17:00:16 -0500

    on load

---
+++ script.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+  $("#myBtn").click(function() {
+    $("#myModal").modal();
+  });
+});
+
+
+// window.onload = event => {
+//   console.log("page is fully loaded");
+// };
+
+// popModal(){
+// $('#myModal').modal('show');
+// };
+
+// var modalEl = $("#exampleModal");
+// function popModal() {
+//   alert("Page is loaded");
+//   modalEl.toggle();
+//   console.log("ahhhh!");
+// }
+
+// $("#myModal").on(load, function() {
+//   $("#myInput").trigger("focus");
+// });
